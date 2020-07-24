84d8335ce69651a3eaeaebbafae037cc20a881af
Madhav Chopra <madhavchopra99@gmail.com>
2020-07-24 22:02:35 +0530

    host added

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ohbe@gs6gt80vnc^fw1@ijw@o+kmk6dnnwuf-!t&9r!+lpk%$*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['blogsite13.herokuapp.com','127.0.0.1']
 
 
 # Application definition
