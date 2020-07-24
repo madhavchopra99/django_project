b111990bc9eb1d96405aa837007835523d4b2067
Madhav Chopra <madhavchopra99@gmail.com>
2020-07-24 22:35:39 +0530

    settings changed

--- django_project/urls.py
+++ django_project/urls.py
@@ -45,8 +45,7 @@ urlpatterns = [
          auth_views.PasswordResetCompleteView.as_view(
              template_name='users/password_reset_complete.html'),
          name='password_reset_complete'),
-] += static(settings.MEDIA_URL,
-            document_root=settings.MEDIA_ROOT)
+]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
