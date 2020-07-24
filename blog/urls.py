14d38faaed22dac8c38c9526af29f57609aca1c1
Madhav Chopra <madhavchopra99@gmail.com>
2020-07-24 22:51:33 +0530

    Signed-off-by: Madhav Chopra <madhavchopra99@gmail.com>

--- blog/urls.py
+++ blog/urls.py
@@ -19,12 +19,16 @@ urlpatterns = [
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('post/search/', PostSearchListView.as_view(), name='post-search'),
 
-    path('post/<int:id>/comments/', CommentListView.as_view(), name='post-comments'),
+    path('post/<int:id>/comments/',
+         CommentListView.as_view(), name='post-comments'),
 
-    path('post/<int:id>/comments/new', CommentCreateView.as_view(), name='post-comment-create'),
-    path('post/<int:id>/comments/<int:pk>/update', CommentUpdateView.as_view(), name='post-comment-update'),
+    path('post/<int:id>/comments/new',
+         CommentCreateView.as_view(), name='post-comment-create'),
+    path('post/<int:id>/comments/<int:pk>/update',
+         CommentUpdateView.as_view(), name='post-comment-update'),
 
-    path('post/<int:id>/comments/<int:pk>/delete', CommentDeleteView.as_view(), name='post-comment-delete'),
+    path('post/<int:id>/comments/<int:pk>/delete',
+         CommentDeleteView.as_view(), name='post-comment-delete'),
 
 
 
