c08e938007a6a6d5776ff768812a95ef65a24e8c
Madhav Chopra <madhavchopra99@gmail.com>
2020-07-24 23:13:40 +0530

    minor changes

--- blog/views.py
+++ blog/views.py
@@ -26,55 +26,55 @@ class UserPostListView(ListView):
 
 
 class PostListView(ListView):
-    """docstring for PostListView"""
-    model = Post
-    template_name = 'blog/home.html'  # <app-name>/<model-name>_<view-type>.html
-    context_object_name = 'posts'
-    ordering = ['-date_posted']
-    paginate_by = 5
+	"""docstring for PostListView"""
+	model = Post
+	template_name = 'blog/home.html'  # <app-name>/<model-name>_<view-type>.html
+	context_object_name = 'posts'
+	ordering = ['-date_posted']
+	paginate_by = 5
 
 
 class PostDetailView(DetailView):
-    """docstring for PostListView"""
-    model = Post
+	"""docstring for PostListView"""
+	model = Post
 
 
 class PostCreateView(LRmixin, CreateView):
-    model = Post
-    fields = ['title', 'content']
+	model = Post
+	fields = ['title', 'content']
 
-    def form_valid(self, form):
-        form.instance.author = self.request.user
-        return super().form_valid(form)
+	def form_valid(self, form):
+		form.instance.author = self.request.user
+		return super().form_valid(form)
 
 
 class PostUpdateView(LRmixin, UPTmixin, UpdateView):
-    model = Post
-    fields = ['title', 'content']
+	model = Post
+	fields = ['title', 'content']
 
-    def form_valid(self, form):
-        form.instance.author = self.request.user
-        return super().form_valid(form)
+	def form_valid(self, form):
+		form.instance.author = self.request.user
+		return super().form_valid(form)
 
-    def test_func(self):
-        post = self.get_object()
-        if self.request.user == post.author:
-            return True
+	def test_func(self):
+		post = self.get_object()
+		if self.request.user == post.author:
+			return True
 
-        return False
+		return False
 
 
 class PostDeleteView(LRmixin, UPTmixin, DeleteView):
-    """docstring for PostListView"""
-    model = Post
-    success_url = '/'
+	"""docstring for PostListView"""
+	model = Post
+	success_url = '/'
 
-    def test_func(self):
-        post = self.get_object()
-        if self.request.user == post.author:
-            return True
+	def test_func(self):
+		post = self.get_object()
+		if self.request.user == post.author:
+			return True
 
-        return False
+		return False
 
 
 class PostSearchListView(ListView):
@@ -105,63 +105,63 @@ class PostSearchListView(ListView):
 
 
 class CommentListView(ListView):
-    model = Comment
-    context_object_name = 'object'
-    paginate_by = 10
+		model = Comment
+		context_object_name = 'object'
+		paginate_by = 10
 
-    def access_id(self):
-        id = self.kwargs.get('id')
+		def access_id(self):
+			id = self.kwargs.get('id')
 
-    def get_queryset(self):
-        post_ = get_object_or_404(Post, id=self.kwargs.get('id'))
-        return Comment.objects.filter(post=post_).order_by('-date_posted')
+		def get_queryset(self):
+		 	post_ = get_object_or_404(Post, id=self.kwargs.get('id'))
+		 	return Comment.objects.filter(post=post_).order_by('-date_posted')
 
 
 class CommentCreateView(LRmixin, CreateView):
-    model = Comment
-    fields = ['content']
+	model = Comment
+	fields = ['content']
 
-    def form_valid(self, form):
-        form.instance.author = self.request.user
-        form.instance.post = get_object_or_404(Post, id=self.kwargs.get('id'))
-        return super().form_valid(form)
+	def form_valid(self, form):
+		form.instance.author = self.request.user
+		form.instance.post = get_object_or_404(Post, id=self.kwargs.get('id'))
+		return super().form_valid(form)
 
 
 class CommentUpdateView(LRmixin, UPTmixin, UpdateView):
-    model = Comment
-    fields = ['content']
+	model = Comment
+	fields = ['content']
 
-    def form_valid(self, form):
-        form.instance.author = self.request.user
-        form.instance.post = get_object_or_404(Post, id=self.kwargs.get('id'))
-        return super().form_valid(form)
+	def form_valid(self, form):
+		form.instance.author = self.request.user
+		form.instance.post = get_object_or_404(Post, id=self.kwargs.get('id'))
+		return super().form_valid(form)
 
-    def test_func(self):
-        comment = self.get_object()
-        if self.request.user == comment.author:
-            return True
+	def test_func(self):
+		comment = self.get_object()
+		if self.request.user == comment.author:
+			return True
 
-        return False
+		return False
 
 
 class CommentDeleteView(LRmixin, UPTmixin, DeleteView):
-    """docstring for PostListView"""
-    model = Comment
+	"""docstring for PostListView"""
+	model = Comment
 
-    def test_func(self):
-        post = self.get_object()
-        if self.request.user == post.author:
-            return True
+	def test_func(self):
+		post = self.get_object()
+		if self.request.user == post.author:
+			return True
 
-        return False
+		return False
 
-    def get_success_url(self):
-        id = 0
-        id = self.kwargs.get('id')
-        return reverse('post-detail', kwargs={'pk': id})
-        pass
+	def get_success_url(self):
+		id = 0
+		id = self.kwargs.get('id')
+		return reverse('post-detail', kwargs={'pk': id})
+		pass
 
 
 def about(request):
-    context = {'title': 'About'}
-    return render(request, 'blog/about.html', context)
+	context = {'title': 'About'}
+	return render(request, 'blog/about.html', context)
--- users/signals.py
+++ users/signals.py
@@ -7,14 +7,14 @@ import os
 
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-    pass
+	if created:
+		Profile.objects.create(user=instance)
+	pass
 
 
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
-    instance.profile.save()
+	instance.profile.save()
 
 # @receiver(models.signals.post_delete, sender=MediaFile)
 # def auto_delete_file_on_delete(sender, instance, **kwargs):
@@ -29,16 +29,16 @@ def save_profile(sender, instance, **kwargs):
 
 @receiver(pre_save, sender=Profile)
 def auto_delete_old_pics_on_change(sender, instance, **kwargs):
-    if not instance.pk:
-        return False
+	if not instance.pk:
+		return False
 
-    try:
-        old_file = Profile.objects.get(pk=instance.pk).image
+	try:
+		old_file = Profile.objects.get(pk=instance.pk).image
 
-    except Profile.DoesNotExist:
-        return False
+	except Profile.DoesNotExist:
+		return False
 
-    new_file = instance.image
-    if not old_file == new_file:
-        if os.path.isfile(old_file.path):
-            os.remove(old_file.path)
+	new_file = instance.image
+	if not old_file == new_file:
+		if os.path.isfile(old_file.path):
+			os.remove(old_file.path)
